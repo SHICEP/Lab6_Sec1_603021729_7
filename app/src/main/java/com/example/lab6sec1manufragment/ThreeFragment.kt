a75d8eaa2bc638bdbe657b2060f871fe0ae40bfe
Ice <Paweesuda75@hotmail.co.th>
2019-09-02 18:01:42 +0700

    Lab6_Sec1_603021729_7

---
+++ app/src/main/java/com/example/lab6sec1manufragment/ThreeFragment.kt
@@ -0,0 +1,31 @@
+package com.example.lab6sec1manufragment
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_three2.view.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ThreeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view :View = inflater.inflate(R.layout.fragment_three2,container,false)
+        view.btnClickFragThree.setOnClickListener(){
+            val  toast:Toast = Toast.makeText(context,"Click On Fragment Three",Toast.LENGTH_SHORT)
+            toast.show()
+        }
+        return view
+    }
+
+
+}
